25106f3d4bfa8ff9b12ee08d14c6916dbf60913e
KyAnhVo <tommyvo0406@gmail.com>
2024-03-12 19:59:17 -0500

    Change file + comments in readFile.asm

---
+++ ReadFile.asm
@@ -0,0 +1,4 @@
+# openFile: 		input is a string containing path to file, then returns file handler
+# getRandCharacter: 	input is a file handler of format Word1Word2...Wordn where every word is 5 chars long
+#			then returns a random letter in that file.
+# closeFile:		input is a file handler, close the file with no output.
